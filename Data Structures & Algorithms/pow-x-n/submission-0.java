c3f9b82d811e6fa401d0bd1ecc3424b3779491a4
Juzer Ali <1373803+juzerali@users.noreply.github.com>
2026-05-19 12:33:33 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(x == 0) return 0;
+        if(n == 1) return x;
+        if(n == -1) return 1/x;
+
+        if(n > 0) {
+            return x*myPow(x, n-1);
+        } else {
+            return myPow(x, n+1)/x;
+        }
+        
+    }
+}
