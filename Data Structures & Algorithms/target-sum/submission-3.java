29ca307832eabffd2f07a257afbe122492df6cfc
Juzer Ali <1373803+juzerali@users.noreply.github.com>
2026-05-20 07:59:35 +0530

    Add: target-sum - submission-3

---
+++ Data Structures & Algorithms/target-sum/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return findTargetSumWays(nums, target, 0);
+    }
+
+    public int findTargetSumWays(int[] nums, int target, int i) {
+        if(i >= nums.length) return 0;
+        if(i == nums.length - 1) {
+            if(target == 0 && nums[i] == 0) return 2;
+            if(target == nums[i] || target == -nums[i]) return 1;
+            return 0;
+        }
+
+        return findTargetSumWays(nums, target+nums[i], i+1) + findTargetSumWays(nums, target-nums[i], i+1);
+    }
+}
