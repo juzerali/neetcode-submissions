911638f488893d4a8081815a79c1a12bd6be8688
Juzer Ali <1373803+juzerali@users.noreply.github.com>
2026-05-20 10:56:07 +0530

    Add: interleaving-string - submission-2

---
+++ Data Structures & Algorithms/interleaving-string/submission-2.java
@@ -0,0 +1,44 @@
+class Solution {
+    Boolean[][] memo;
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int l3 = s3.length();
+
+        memo = new Boolean[l1+1][l2+1];
+
+        if(l3 != l1+l2) return false;
+
+        return isInterleave(s1, s2, s3, 0, 0);
+        
+    }
+
+    public boolean isInterleave(String s1, String s2, String s3, int i, int j) {
+        boolean b1 = i >= s1.length();
+        boolean b2 = j >= s2.length();
+
+        if(b1 && b2) return true;
+
+        if(memo[i][j] != null) return memo[i][j];
+
+        if(!b1) {
+            b1 = s1.charAt(i) == s3.charAt(i+j) && isInterleave(s1, s2, s3, i+1, j);
+            if(b1) {
+                memo[i][j] = true;
+                return b1;
+            }
+        }
+
+        if(!b2) {
+            b2 = s2.charAt(j) == s3.charAt(i+j) && isInterleave(s1, s2, s3, i, j+1);
+            if(b2) {
+                memo[i][j] = true;
+                return b2;
+            }
+        }
+        
+        memo[i][j] = false;
+        return false;
+    }
+}
