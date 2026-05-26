f37a6437878bd24a30de8767592661abf9922ef8
Juzer Ali <1373803+juzerali@users.noreply.github.com>
2026-05-26 20:42:00 +0530

    Add: insert-greatest-common-divisors-in-linked-list - submission-0

---
+++ Data Structures & Algorithms/insert-greatest-common-divisors-in-linked-list/submission-0.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        head.next = new ListNode(gcd(head.val, head.next.val), head.next);
+        insertGreatestCommonDivisors(head.next.next);
+        return head;
+    }
+
+
+    private int gcd(int a, int b) {
+        while (b > 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
